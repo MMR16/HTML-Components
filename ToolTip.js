0ace91832875759936d0e3bdcc7fc550a138a3d1
MMR16 <MMR16RADY@YAHOO.COM>
2020-09-11 06:53:51 +0200

    working with DOM
    working with DOM, adding events & some style

--- ToolTip.js
+++ ToolTip.js
@@ -1,9 +1,33 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
-        console.log(" First Component Works Well");
+        this._tooltipContainer;
+        // console.log(" First Component Works Well");
     }
+
+    //connectedCallback method used to Work With DOM When This method called 
+    connectedCallback() {
+        const tooltipIcon = document.createElement('span');
+        tooltipIcon.innerText = '(?)'
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
+
+        this.appendChild(tooltipIcon);
+    }
+    _showTooltip() {
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.innerText = 'This Is A Tooltip Text';
+        this._tooltipContainer.style.fontSize = '30px';
+        this._tooltipContainer.style.color = 'red';
+        this.appendChild(this._tooltipContainer);
+    }
+    _hideTooltip() {
+        // this._tooltipContainer.remove();
+        this.removeChild(this._tooltipContainer);
+
+    }
+
 }
 
-///the name of tag name (html elemnt) , the name of constructor (class) of that element & must be aconstructor not any other method
+///the name of tag name (HTML element) , the name of constructor (class) of that element & must be aconstructor not any other method.
 customElements.define('mmr-tooltip', Tooltip);
