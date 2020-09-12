368d9d7b7e3cf4bc2d7963732916deffb6f329ce
MMR16 <MMR16RADY@YAHOO.COM>
2020-09-12 05:21:04 +0200

    using style in shadow DOM

--- ToolTip.js
+++ ToolTip.js
@@ -6,6 +6,14 @@ class Tooltip extends HTMLElement {
         ///////////Shadow Dom \\\\\\\\\ to add style that doesn't affected by any css file or any style
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
+                                     <style>
+                                     div{
+                                       background-color:black; 
+                                       color: red;
+                                       position: absolute;
+                                       z-index=10;
+                                     }
+                                     </style>
                                      <slot>some default </slot>
                                      <!-- any thing between slot will useed as default if not overwrite it -->
                                      <span>(?)</span>`;
@@ -23,10 +31,6 @@ class Tooltip extends HTMLElement {
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
         this._tooltipContainer.textContent = 'This Is A Tooltip Text';
-        this._tooltipContainer.style.backgroundColor = 'black';
-        this._tooltipContainer.style.color = 'red';
-        this._tooltipContainer.style.position = 'absolute';
-        this._tooltipContainer.style.zIndex = '10';
         this.shadowRoot.appendChild(this._tooltipContainer); ///////////Shadow Dom \\\\\\\\\ 
     }
     _hideTooltip() {
