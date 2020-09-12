8d875471e433823c4a2c79d6f7e0d758b5732255
MMR16 <MMR16RADY@YAHOO.COM>
2020-09-12 04:23:08 +0200

    adding some Styles

--- ToolTip.js
+++ ToolTip.js
@@ -8,17 +8,20 @@ class Tooltip extends HTMLElement {
     //connectedCallback method used to Work With DOM When This method called 
     connectedCallback() {
         const tooltipIcon = document.createElement('span');
-        tooltipIcon.innerText = '(?)'
+        tooltipIcon.textContent = '(?)'
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
 
         this.appendChild(tooltipIcon);
+        this.style.position = 'relative'
     }
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.innerText = 'This Is A Tooltip Text';
-        this._tooltipContainer.style.fontSize = '30px';
+        this._tooltipContainer.textContent = 'This Is A Tooltip Text';
+        this._tooltipContainer.style.backgroundColor = 'black';
         this._tooltipContainer.style.color = 'red';
+        this._tooltipContainer.style.position = 'absolute';
+        this._tooltipContainer.style.zIndex = '10';
         this.appendChild(this._tooltipContainer);
     }
     _hideTooltip() {
